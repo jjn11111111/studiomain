42e1a13379debb04a21e0d7a8d268920891f3aa8
john Jeremy Norman <johnjeremynorman@gmail.com>
2025-10-29 19:03:35 +0000

    do i need those additonal packages in in the dev. nix file

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,8 +5,6 @@
   channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.flutter
-    pkgs.busybox
     pkgs.run
     pkgs.nodejs_20
     pkgs.zulu
