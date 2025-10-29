839d3d2677ddcda69337613a9137430d342efbf5
john Jeremy Norman <johnjeremynorman@gmail.com>
2025-10-29 03:05:20 +0000

    can you engage git push to commit the changes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,7 +5,7 @@
   channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.deno
+    pkgs.flutter
     pkgs.busybox
     pkgs.run
     pkgs.nodejs_20
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 
+
 # Firebase Studio
 
 ---
