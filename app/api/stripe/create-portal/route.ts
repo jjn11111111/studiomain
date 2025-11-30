7c82b11efc523daaef03b28e17529d3246d055b4
jjn11111111 <100871704+jjn11111111@users.noreply.github.com>
2025-11-30 12:26:21 -0500

    Update route.ts

--- app/api/stripe/create-portal/route.ts
+++ app/api/stripe/create-portal/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 // CHANGE THESE LINES:
 import { getSupabaseServerClient } from '@/lib/supabaseServer'
-import { getOrCreateCustomer, stripe } from '@/lib/stripe'
 
 export async function POST() {
   // ... rest of the code
